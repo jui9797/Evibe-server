f52aedc214e8cf8a85c0c000d2a946c1c2204fb2
Lenovo <jannatjui414@gmail.com>
2025-07-01 14:59:40 +0600

    add api for update event

--- index.js
+++ index.js
@@ -209,6 +209,46 @@ async function run() {
         res.status(500).send({ message: "Server Error" });
       }
     });
+    // delete event
+    app.delete("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const result = await eventCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+        if (result.deletedCount === 1) {
+          res.status(200).json({ message: "Event deleted successfully" });
+        } else {
+          res.status(404).json({ message: "Event not found" });
+        }
+      } catch (error) {
+        res.status(500).json({ message: "Failed to delete event", error });
+      }
+    });
+
+    // update event by id
+    app.put("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedEvent = req.body;
+
+      try {
+        const result = await eventCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updatedEvent }
+        );
+
+        if (result.modifiedCount > 0) {
+          res.send({ message: "Event updated successfully", modifiedCount: 1 });
+        } else {
+          res
+            .status(404)
+            .send({ message: "Event not found or no changes made" });
+        }
+      } catch (error) {
+        console.error("Error updating event:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
